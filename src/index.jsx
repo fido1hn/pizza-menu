36f42645fda3fe6f353392e781053b9d5ff34367
fido1hn <confidoekeanya@gmail.com>
2025-03-22 04:35:45 +0100

    component intro

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return <h2>Pizza</h2>;
 }
 
 const root = ReactDom.createRoot(document.getElementById("root"));
