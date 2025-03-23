5b5b21df148bb114672c6ab9fd489852ea270a43
fido1hn <confidoekeanya@gmail.com>
2025-03-23 18:15:18 +0100

    reusable components

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,20 @@ function App() {
     <div>
       <h1>Hello React!</h1>
       <Pizza />
+      <Pizza />
+      <Pizza />
     </div>
   );
 }
 
 function Pizza() {
-  return <h2>Pizza</h2>;
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="spinaci pizza" />
+      <h2>Pizza</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
 }
 
 const root = ReactDom.createRoot(document.getElementById("root"));
